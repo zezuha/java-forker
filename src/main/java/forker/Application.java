a336229be9763645e63f12571b00e29f5a7d60fb
PSM <pawel.smigiel@gmail.com>
2019-04-14 18:50:27 +0100

    initial body of Application to read a file

--- src/main/java/forker/Application.java
+++ src/main/java/forker/Application.java
@@ -1,11 +1,16 @@
 package forker;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Application {
 
-    private Logger logger = LoggerFactory.getLogger(Application.class);
+    private static Logger log = LoggerFactory.getLogger(Application.class);
 
     private static final String USAGE = "Usage:\n"
             + "java -jar ./build/java-forker.jar <filename>\n";
@@ -21,10 +26,37 @@ public class Application {
         app.run();
     }
 
+    private static final String FILENAME = "content.txt";
+
     public Application(String fileName) {
-    }
-    
-    private void run() {
+        log.info("Application started");
+        run();
+        log.info("Application finished");
     }
 
+    private void run() {
+        sleep("Application", 15*1000);
+    }
+
+    static void sleep(final String processName, int ms) {
+        log.info(processName + " is running for " + ms/1000 + " seconds");
+        try {
+            Thread.sleep(ms);
+        } catch (InterruptedException ignore) {
+        }
+    }
+
+    static void readFileContent(final String processName) {
+        try {
+	        File file = new File(FILENAME);
+	        BufferedReader br = new BufferedReader(new FileReader(file)); 
+	        String st;
+	        while ((st = br.readLine()) != null) {
+	            System.out.println("file " + FILENAME + " content: " + st);
+	        }
+    	} catch (IOException problem) {
+    		log.error(processName + " was unable to read content of " + FILENAME);
+    	}
+        log.info(processName + " successfully read "+ FILENAME);
+    }
 }
